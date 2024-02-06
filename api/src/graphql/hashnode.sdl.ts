799d6af2bdcc272f69543eb072c1b8fbc252b760
David Thyresson <dthyresson@gmail.com>
2024-02-06 18:12:39 -0500

    Adds SDL descriptions for explorer docs

--- api/src/graphql/hashnode.sdl.ts
+++ api/src/graphql/hashnode.sdl.ts
@@ -1,29 +1,35 @@
 export const schema = gql`
+  "Publication is derived from https://apidocs.hashnode.com/#definition-Publication"
   type Publication {
     isTeam: Boolean
     title: String!
     posts: [Post!]
   }
 
+  "Content is derived from https://apidocs.hashnode.com/#definition-Content"
   type Content {
     markdown: String!
     html: String!
     text: String!
   }
 
+  "OpenGraphMetaData is derived from https://apidocs.hashnode.com/#definition-OpenGraphMetaData"
   type OpenGraphMetaData {
     image: String
   }
 
+  "PostCoverImage is derived from https://apidocs.hashnode.com/#definition-PostCoverImage"
   type PostCoverImage {
     url: String!
   }
 
+  "SEO is derived from https://apidocs.hashnode.com/#definition-SEO"
   type SEO {
     title: String
     description: String
   }
 
+  "Post is derived from https://apidocs.hashnode.com/#definition-Post"
   type Post {
     id: String!
     slug: String!
@@ -40,6 +46,7 @@ export const schema = gql`
     ogMetaData: OpenGraphMetaData
   }
 
+  "Author is derived from https://apidocs.hashnode.com/#definition-User"
   type Author {
     id: String!
     name: String!
@@ -47,6 +54,7 @@ export const schema = gql`
   }
 
   type Query {
+    "Fetches a Publication with latest posts from Hashnode."
     recentPosts(first: Int): Publication! @skipAuth
   }
 `
