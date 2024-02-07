cf400f861857ae6ed81fd5df54d441400be04ca1
Amy Haywood Dutton <me@amyhaywood.com>
2024-02-06 22:27:34 -0600

    ✨ Updated router

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,17 +12,17 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/events" page={EventsPage} name="events" prerender />
-      <Route path="/blog/{slug:string}" page={BlogIndividualPage} name="blogIndividual" prerender />
-      <Route path="/blog" page={BlogPage} name="blog" prerender />
-      <Route path="/newsletter" page={NewsletterPage} name="newsletter" prerender />
-      <Route path="/changelog" page={ChangelogPage} name="changelog" prerender />
-      <Route path="/roadmap" page={RoadmapPage} name="roadmap" prerender />
-      <Route path="/security" page={SecurityPage} name="security" prerender />
-      <Route path="/brand" page={BrandPage} name="brand" prerender />
-      <Route path="/contributors" page={ContributorsPage} name="contributors" prerender />
-      <Route path="/core-team" page={CoreTeamPage} name="coreTeam" prerender />
-      <Route path="/" page={HomePage} name="home" prerender />
+      <Route path="/events" page={EventsPage} name="events" />
+      <Route path="/blog/{slug:string}" page={BlogIndividualPage} name="blogIndividual" />
+      <Route path="/blog" page={BlogPage} name="blog" />
+      <Route path="/newsletter" page={NewsletterPage} name="newsletter" />
+      <Route path="/changelog" page={ChangelogPage} name="changelog" />
+      {/* <Route path="/roadmap" page={RoadmapPage} name="roadmap" /> */}
+      <Route path="/security" page={SecurityPage} name="security" />
+      <Route path="/brand" page={BrandPage} name="brand" />
+      {/* <Route path="/contributors" page={ContributorsPage} name="contributors" /> */}
+      {/* <Route path="/core-team" page={CoreTeamPage} name="coreTeam" /> */}
+      <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
