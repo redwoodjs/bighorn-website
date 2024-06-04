3f3aac5886d81de0b6acb33473b939493128a469
Rob Cameron <cannikin@fastmail.com>
2024-06-04 09:42:57 -0700

    Remove GraphQL caching for hashnode requests

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -1,8 +1,3 @@
-import {
-  createInMemoryCache,
-  useResponseCache,
-} from '@graphql-yoga/plugin-response-cache'
-
 import { createGraphQLHandler } from '@redwoodjs/graphql-server'
 
 import directives from 'src/directives/**/*.{js,ts}'
@@ -12,29 +7,11 @@ import services from 'src/services/**/*.{js,ts}'
 import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
 
-export const cache = createInMemoryCache()
-
 export const handler = createGraphQLHandler({
   loggerConfig: { logger, options: {} },
   directives,
   sdls,
   services,
-  extraPlugins: [
-    useResponseCache({
-      session: () => null,
-      // by default cache all operations for 1 hour
-      ttl: 1_000 * 60 * 60,
-      ttlPerType: {
-        // only cache query operations containing Post for 1 day
-        Post: 1_000 * 60 * 60 * 24,
-      },
-      ttlPerSchemaCoordinate: {
-        // cache for 1 day
-        'Query.recentPosts': 1_000 * 60 * 60 * 24,
-      },
-      cache,
-    }),
-  ],
   onException: () => {
     // Disconnect from your database with an unhandled exception.
     db.$disconnect()
