3ea92df1eb8beb478076b78f0aecff9e62d01f0d
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-07-02 22:23:42 +0100

    remove changelog

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,6 @@ import { Metadata } from '@redwoodjs/web'
 import { Toaster } from '@redwoodjs/web/dist/toast'
 
 import Footer from 'src/components/Footer/Footer'
-import ChangelogSummary from 'src/components/Home/ChangelogSummary/ChangelogSummary'
 import EventSummary from 'src/components/Home/EventSummary/EventSummary'
 import HomeHero from 'src/components/Home/HomeHero/HomeHero'
 import RecentBlogPostsCell from 'src/components/Home/RecentBlogPostsCell'
@@ -39,9 +38,6 @@ const HomePage = () => {
 
       {/* ROADMAP */}
       <RoadmapSummary />
-
-      {/* CHANGELOG */}
-      <ChangelogSummary />
 
       {/* BLOG */}
       <RecentBlogPostsCell />
