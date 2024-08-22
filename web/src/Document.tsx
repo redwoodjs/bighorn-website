4a8d9896c4fa943f9fcb84002a99744ddce77b45
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-08-22 21:52:14 +0100

    put tag in document (#59)

--- web/src/Document.tsx
+++ web/src/Document.tsx
@@ -19,12 +19,32 @@ export const Document: React.FC<DocumentProps> = ({ children, css, meta }) => {
         <Css css={css} />
         <Meta tags={meta} />
 
+        {/* Plausible */}
+        <script
+          src="https://plausible.io/js/script.outbound-links.tagged-events.js"
+          data-domain="redwoodjs.com"
+          defer
+        ></script>
+
         {/* preload hints for css */}
-        <link rel="preload" href="https://use.typekit.net/sjm8rub.css" as="style" />
-        <link rel="preload" href="https://p.typekit.net/p.css?s=1&k=sjm8rub&ht=tk&f=38944&a=8145&app=typekit&e=css" as="style" />
+        <link
+          rel="preload"
+          href="https://use.typekit.net/sjm8rub.css"
+          as="style"
+        />
+        <link
+          rel="preload"
+          href="https://p.typekit.net/p.css?s=1&k=sjm8rub&ht=tk&f=38944&a=8145&app=typekit&e=css"
+          as="style"
+        />
 
         {/* preload hints for images */}
-        <link rel="preload" fetchPriority="high" href="/images/hero.avif" as="image" />
+        <link
+          rel="preload"
+          fetchPriority="high"
+          href="/images/hero.avif"
+          as="image"
+        />
       </head>
       <body>
         <div id="redwood-app">{children}</div>
