53d847853996ded51cb7280cb6d6e24b43ed20a6
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-08-30 21:02:12 +0100

    ensure toc works

--- web/src/components/TableOfContentsItem/TableOfContentsItem.tsx
+++ web/src/components/TableOfContentsItem/TableOfContentsItem.tsx
@@ -2,6 +2,12 @@ export const TableOfContentsItem = ({ node }) => {
   // We replace characters to match the id to make sure it's a valid anchor we're targeting
   const id = node.value
     .toLowerCase()
+    .replace(/\./g, '')
+    .replace(/\//g, '')
+    .replace(/\'/g, '')
+    .replace(/\</g, '')
+    .replace(/\>/g, '')
+    .replace(/\@/g, '')
     .replace(/[^\w-]/g, '-') // replace non-word characters with a dash
     .replace(/-+/g, '-') // replace multiple dashes with a single one
     .replace(/-+$/, '') // remove any dashes at the end of the string
