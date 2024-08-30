1bca0317ca95ec8e36e07243b1818a4770f3f03f
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-08-30 21:10:54 +0100

    scroll margin

--- web/src/layouts/UpgradeGuideLayout/UpgradeGuideLayout.tsx
+++ web/src/layouts/UpgradeGuideLayout/UpgradeGuideLayout.tsx
@@ -12,7 +12,7 @@ const UpgradeGuideLayout = ({ children }: UpgradeGuideLayoutProps) => {
       <div className="fixed top-0 z-nav w-full border-b-1 border-maiTai bg-white px-5 py-2 md:px-page dark:bg-licorice">
         <Nav />
       </div>
-      <div className="pt-[77px]">{children}</div>
+      <div className="scroll-margin-container pt-[77px]">{children}</div>
       <div className="border-t-1 border-t-maiTai py-20 ">
         <Newsletter />
       </div>
