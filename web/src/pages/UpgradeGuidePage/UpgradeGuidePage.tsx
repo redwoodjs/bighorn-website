f346bc36466a30ec6feb4d9d3585cfd40173437f
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-09-02 13:44:50 +0100

    basic upgrade guides list page

--- web/src/pages/UpgradeGuidePage/UpgradeGuidePage.tsx
+++ web/src/pages/UpgradeGuidePage/UpgradeGuidePage.tsx
@@ -1,22 +1,42 @@
-import { Link, routes } from "@redwoodjs/router";
-import { Metadata } from "@redwoodjs/web";
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+import { data as dataV8 } from 'src/content/upgrades/v8/data'
+import { prettifyDate } from 'src/helpers/DateHelpers'
 
 const UpgradeGuidePage = () => {
   return (
     <>
       <Metadata title="UpgradeGuide" description="UpgradeGuide page" />
 
-      <h1>UpgradeGuidePage</h1>
-      <p>
-        Find me in{" "}
-        <code>./web/src/pages/UpgradeGuidePage/UpgradeGuidePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>upgradeGuide</code>, link to me with `
-        <Link to={routes.upgradeGuide()}>UpgradeGuide</Link>`
-      </p>
+      <section className="my-12">
+        <div className="px-5 md:pl-page md:pr-0">
+          <h2 className="section-heading mb-6">Upgrade Guides</h2>
+          <h3 className="section-subheading">
+            Bringing you the latest and greatest from RedwoodJS
+          </h3>
+        </div>
+        <div className="mt-12 flex flex-col gap-6 px-5 md:pl-page md:pr-0">
+          <article>
+            <h3 className="mb-2 text-sm font-bold uppercase text-maiTai">
+              {prettifyDate(dataV8.publishedAt)}
+            </h3>
+            <div className="mb-3 flex flex-col md:flex-row md:items-center md:justify-between">
+              <h2 className="mb-2 font-sans text-2xl font-bold md:mb-0">
+                <Link
+                  to={routes.upgradeGuideV8()}
+                  className="text-black hover:text-christi dark:text-white dark:hover:text-sulu"
+                >
+                  {dataV8.title}
+                </Link>
+              </h2>
+            </div>
+            <p className="mb-3 text-battleshipGray">{dataV8.brief}</p>
+          </article>
+        </div>
+      </section>
     </>
-  );
-};
+  )
+}
 
-export default UpgradeGuidePage;
+export default UpgradeGuidePage
