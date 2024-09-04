5f025301e9dd78fb73de00694281f773e3d1d0ea
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-09-04 02:35:25 +0100

    update links

--- web/src/content/upgrades/v8/data.ts
+++ web/src/content/upgrades/v8/data.ts
@@ -7,6 +7,6 @@ export const data = {
     'Version 8 is out with some cool new features like background jobs! Upgrading should be easy with this guide to help you along the way.',
   tags: ['v8'],
   publishedAt: new Date('2024-09-04T00:10:00.000Z'),
-  updatedAt: new Date('2024-09-04T00:10:00.000Z'),
+  updatedAt: new Date('2024-09-04T01:35:00.000Z'),
   author: JoshGMWalker,
 }
