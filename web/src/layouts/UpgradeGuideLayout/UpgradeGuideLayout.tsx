d171e5c601491001884814268feb5b6a2aaf8e2c
Tobbe Lundberg <tobbe@tlundberg.com>
2025-06-19 15:22:17 +0200

    fix(layout): Make room for banner on v8 upgrade guide page

--- web/src/layouts/UpgradeGuideLayout/UpgradeGuideLayout.tsx
+++ web/src/layouts/UpgradeGuideLayout/UpgradeGuideLayout.tsx
@@ -9,7 +9,7 @@ type UpgradeGuideLayoutProps = {
 const UpgradeGuideLayout = ({ children }: UpgradeGuideLayoutProps) => {
   return (
     <>
-      <div className="fixed top-0 z-nav w-full border-b-1 border-maiTai bg-white px-5 py-2 md:px-page dark:bg-licorice">
+      <div className="fixed top-10 z-nav w-full border-b-1 border-maiTai bg-white px-5 py-2 md:px-page dark:bg-licorice">
         <Nav />
       </div>
       <div className="scroll-margin-container pt-[77px]">{children}</div>
